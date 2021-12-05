03fc3298bc3b1ba6819a7dcd4c739409cb7d81a5
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-12-05 15:57:59 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,142 @@
+from TikTokApi import TikTokApi
+
+import http.client as httplib
+import httplib2
+import os
+import random
+import time
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+httplib2.RETRIES = 1
+
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+  httplib.IncompleteRead, httplib.ImproperConnectionState,
+  httplib.CannotSendRequest, httplib.CannotSendHeader,
+  httplib.ResponseNotReady, httplib.BadStatusLine)
+
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+CLIENT_SECRETS_FILE = 'client_secret_927906389656-pmriqnirjeoolc3295mjum271drlcn8f.apps.googleusercontent.com.json'
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
+
+# Authorize the request and store authorization credentials.
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+def initialize_upload(youtube, options):
+  tags = None
+  if options.keywords:
+    tags = options.keywords.split(',')
+
+  body=dict(
+    snippet=dict(
+      title=options.title,
+      description=options.description,
+      tags=tags,
+      categoryId=options.category
+    ),
+    status=dict(
+      privacyStatus=options.privacyStatus
+    )
+  )
+
+  # Call the API's videos.insert method to create and upload the video.
+  insert_request = youtube.videos().insert(
+    part=','.join(body.keys()),
+    body=body,
+    # The chunksize parameter specifies the size of each chunk of data, in
+    # bytes, that will be uploaded at a time. Set a higher value for
+    # reliable connections as fewer chunks lead to faster uploads. Set a lower
+    # value for better recovery on less reliable connections.
+    #
+    # Setting 'chunksize' equal to -1 in the code below means that the entire
+    # file will be uploaded in a single HTTP request. (If the upload fails,
+    # it will still be retried where it left off.) This is usually a best
+    # practice, but if you're using Python older than 2.6 or if you're
+    # running on App Engine, you should set the chunksize to something like
+    # 1024 * 1024 (1 megabyte).
+    media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+  )
+
+  resumable_upload(insert_request)
+
+# This method implements an exponential backoff strategy to resume a
+# failed upload.
+def resumable_upload(request):
+  response = None
+  error = None
+  retry = 0
+  while response is None:
+    try:
+      print('Uploading file...')
+      status, response = request.next_chunk()
+      if response is not None:
+        if 'id' in response:
+          print('Video id "%s" was successfully uploaded.' % response['id'])
+        else:
+          exit('The upload failed with an unexpected response: %s' % response)
+    except HttpError:
+      error = 'A retriable error occurred'
+
+    if error is not None:
+      print(error)
+      retry += 1
+      if retry > MAX_RETRIES:
+        exit('No longer attempting to retry.')
+
+      max_sleep = 2 ** retry
+      sleep_seconds = random.random() * max_sleep
+      print('Sleeping %f seconds and then retrying...' % sleep_seconds)
+      time.sleep(sleep_seconds)
+
+class Namespace:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+#==============================================================#
+
+api = TikTokApi.get_instance()
+results = 1000 ##MAX. 2000##
+
+# Since TikTok changed their API you need to use the custom_verifyFp option. 
+# In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
+trending = api.by_trending(count=results, custom_verifyFp="verify_kwsmfv4f_imbgSEA4_SGCR_4zH8_B6yp_DwuXSdlILamu")
+
+device_id = api.generate_device_id()
+
+#,custom_did=device_id)
+youtube = get_authenticated_service()
+
+for x in range(100000000000000000000000000000000):
+	trending = api.by_trending(results)
+	for i in trending:
+		print(i['id'])
+		video_bytes = api.get_video_by_tiktok(i, custom_device_id=device_id)
+		with open(i['id'] + ".mp4", "wb") as out:
+    			out.write(video_bytes)
+		file = i['id'] + ".mp4"
+		title = 'TikTok by @' + i['author']['nickname']
+		args = Namespace(file=file, title=title, description=i['desc'], category='22', keywords='tik,tok,tiktok,pokesi,trending,popular,funny,but,not,funny,at,all', privacyStatus='public')
+		try:
+			#if not(i['author']['nickname'] == 'TikTok'):
+    			initialize_upload(youtube, args)
+		except HttpError:
+    			print('An HTTP error occurred')
+		os.remove(file)
+		time.sleep(10)
